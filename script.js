6557be233c64770efa1078746915876697ef93ba
SeprU <polish.potato02@gmail.com>
2023-09-27 20:39:16 +0200

    ehe

--- script.js
+++ script.js
@@ -19,3 +19,58 @@ let calcScrollValue = () => {
 
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
+
+
+
+
+
+
+
+
+
+// Pobierz elementy HTML
+const notification = document.querySelector(".notification");
+const overlay = document.querySelector(".overlay");
+const showNotificationButton = document.getElementById("showNotification");
+const closeIcon = document.querySelector(".close-icon");
+const dontShowAgainButton = document.getElementById("dontShowAgain");
+
+// Funkcja do pokazywania powiadomienia
+function showNotification() {
+    notification.style.display = "block";
+    overlay.style.display = "block";
+}
+
+// Funkcja do ukrywania powiadomienia
+function hideNotification() {
+    notification.style.display = "none";
+    overlay.style.display = "none";
+}
+
+// Obsługa kliknięcia przycisku "CFG"
+showNotificationButton.addEventListener("click", showNotification);
+
+// Obsługa kliknięcia ikonki X
+closeIcon.addEventListener("click", hideNotification);
+
+// Obsługa kliknięcia przycisku "Nie pokazuj ponownie"
+dontShowAgainButton.addEventListener("click", () => {
+    hideNotification();
+    // Tutaj możesz dodać kod do zapisania informacji, żeby nie pokazywać powiadomienia ponownie
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
